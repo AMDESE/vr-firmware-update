5ef932f1e0d0a3e490697ab89f5be974d052dd5d
Abinaya <abinaya.dhandapani@amd.com>
2023-02-14 05:44:11 -0600

    new VR framework design for infineon and renesas VR's
    Signed-off-by: Abinaya <abinaya.dhandapani@amd.com>

---
+++ inc/vr_update.hpp
@@ -0,0 +1,236 @@
+/*
+* vr-update.hpp
+*
+* Created on: Nov 9, 2022
+* 	Author: alkulkar
+*/
+
+
+#ifndef VR_UPDATE_H_
+#define VR_UPDATE_H_
+
+
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <string>
+#include <filesystem>
+#include <fstream>
+#include <cstring>
+#include <stdint.h>
+#include <algorithm>
+#include <dirent.h>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include <filesystem>
+#include <phosphor-logging/log.hpp>
+
+extern "C"
+{
+#include <unistd.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <i2c/smbus.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+}
+
+#define COMMAND_OUTPUT_LEN  (50)
+#define	MPS2861				("MPS2861")
+#define	MPS2862				("MPS2862")
+
+#define GEN2                  ("GEN2")
+#define GEN3                  ("GEN3")
+#define RAA229613             ("RAA229613")
+#define RAA229625             ("RAA229625")
+#define RAA229620             ("RAA229620")
+#define RAA229621             ("RAA229621")
+#define ISL68220              ("ISL68220")
+#define RENESAS               ("RENESAS")
+#define INFINEON_XDPE         ("XDPE")
+#define INFINEON_TDA          ("TDA")
+#define PATCH                 ("PATCH")
+
+#define SOCKET_0              ("P0")
+#define SOCKET_1              ("P1")
+
+#define BYTE_COUNT_5		  (5)
+#define BYTE_COUNT_4		  (4)
+#define BYTE_COUNT_3		  (3)
+#define BYTE_COUNT_2          (2)
+#define SHIFT_24              (24)
+#define SHIFT_16              (16)
+#define SHIFT_8               (8)
+#define SHIFT_6               (6)
+#define SHIFT_4               (4)
+
+#define SUCCESS               (0)
+#define FAILURE               (-1)
+
+#define INDEX_0               (0)
+#define INDEX_1               (1)
+#define INDEX_2               (2)
+#define INDEX_3               (3)
+#define INDEX_4               (4)
+#define INDEX_5               (5)
+#define INDEX_6               (6)
+#define INDEX_8               (8)
+#define INDEX_10              (10)
+#define INDEX_12              (12)
+#define INDEX_14              (14)
+#define INDEX_32              (32)
+#define INDEX_48              (48)
+
+#define STATUS_BIT_0          (0)
+#define STATUS_BIT_1          (1)
+#define STATUS_BIT_2          (2)
+#define STATUS_BIT_3          (3)
+#define STATUS_BIT_4          (4)
+#define STATUS_BIT_5          (5)
+#define STATUS_BIT_6          (6)
+#define STATUS_BIT_7          (7)
+#define STATUS_BIT_8          (8)
+#define INT_255               (0xFF)
+#define INT_15                (0x0F)
+#define SLAVE_13              (0x13)
+#define SLAVE_14              (0x14)
+#define SLAVE_15              (0x15)
+
+#define BASE_16               (16)
+#define FILE_PATH_SIZE        (256)
+#define MIN_WAIT_TIME         (10000)
+
+#define ISL_DRIVER_PATH       ("/sys/bus/i2c/drivers/isl68137/")
+#define MPS_DRIVER_PATH       ("/sys/bus/i2c/drivers/MP2862/")
+#define XDPE_DRIVER_PATH      ("/sys/bus/i2c/drivers/xdpe12284/")
+
+#define SLEEP_1               (1000000)
+#define SLEEP_2               (2000000)
+#define SLEEP_1000            (1000)
+
+class vr_update {
+
+protected:
+    uint16_t SlaveAddress;
+    uint16_t BusNumber;
+    uint32_t Crc;
+    std::string Processor;
+    std::string MachineName;
+    std::string Model;
+    std::string ConfigFilePath;
+    std::string DriverPath;
+    int fd;
+    char updateFilePath[FILE_PATH_SIZE];
+
+public:
+ vr_update(std::string Processor,
+          std::string MachineName,uint32_t Crc,std::string Model,
+          uint16_t SlaveAddress,std::string ConfigFilePath);
+
+ static vr_update* CreateVRFrameworkObject(std::string Model,
+              uint16_t SlaveAddress, uint32_t Crc, std::string Processor,
+              std::string MachineName, std::string configFilePath,std::string UpdateType);
+
+    virtual bool isUpdatable() = 0;
+    virtual bool findBusNumber();
+    virtual bool openI2cDevice();
+    virtual void closeI2cDevice();
+    virtual bool UpdateFirmware() = 0;
+    virtual bool crcCheckSum() = 0;
+    virtual bool ValidateFirmware() = 0;
+
+};
+
+
+/*class vr_update_mps: public vr_update
+{
+
+public:
+    vr_update_mps(std::string Processor,
+          std::string MachineName,uint32_t Crc,std::string Model,
+          uint16_t SlaveAddress,std::string ConfigFilePath);
+
+    virtual bool crcCheckSum();
+    virtual bool isUpdatable();
+    virtual bool UpdateFirmware();
+    virtual bool ValidateFirmware(){return true;};
+};*/
+
+class vr_update_infineon_xdpe: public vr_update
+{
+
+
+public:
+    vr_update_infineon_xdpe(std::string Processor,
+          std::string MachineName,uint32_t Crc,std::string Model,
+          uint16_t SlaveAddress,std::string ConfigFilePath);
+
+    virtual bool crcCheckSum();
+    virtual bool isUpdatable();
+    virtual bool UpdateFirmware();
+    virtual bool ValidateFirmware();
+};
+
+class vr_update_infineon_tda: public vr_update
+{
+
+protected:
+    int NextImgPtr;
+
+public:
+    vr_update_infineon_tda(std::string Processor,
+          std::string MachineName,uint32_t Crc,std::string Model,
+          uint16_t SlaveAddress,std::string ConfigFilePath);
+
+    virtual bool crcCheckSum();
+    virtual bool isUpdatable();
+    virtual bool UpdateFirmware();
+    virtual bool ValidateFirmware();
+    virtual bool findBusNumber();
+};
+
+class vr_update_renesas_gen3: public vr_update
+{
+
+public:
+    vr_update_renesas_gen3(std::string Processor,
+          std::string MachineName,uint32_t Crc,std::string Model,
+          uint16_t SlaveAddress,std::string ConfigFilePath);
+
+    virtual bool crcCheckSum();
+    virtual bool isUpdatable();
+    virtual bool UpdateFirmware();
+    virtual bool ValidateFirmware();
+};
+
+class vr_update_renesas_gen2: public vr_update
+{
+
+public:
+    vr_update_renesas_gen2(std::string Processor,
+          std::string MachineName,uint32_t Crc,std::string Model,
+          uint16_t SlaveAddress,std::string ConfigFilePath);
+
+    virtual bool crcCheckSum();
+    virtual bool isUpdatable();
+    virtual bool UpdateFirmware();
+    virtual bool ValidateFirmware();
+};
+
+class vr_update_renesas_patch: public vr_update
+{
+
+public:
+    vr_update_renesas_patch(std::string Processor,
+          std::string MachineName,uint32_t Crc,std::string Model,
+          uint16_t SlaveAddress,std::string ConfigFilePath);
+
+    virtual bool crcCheckSum();
+    virtual bool isUpdatable();
+    virtual bool UpdateFirmware();
+    virtual bool ValidateFirmware();
+};
+
+#endif
+
---
+++ src/vr_update.cpp
@@ -0,0 +1,190 @@
+/*
+* vr-update.cpp
+*
+* Created on: Nov 10, 2022
+* Author: Abinaya Dhandapani
+*/
+
+#include"vr_update.hpp"
+
+#define MODEL			("Model")
+#define SLAVE_ADDRESS	("SlaveAddress")
+#define PROCESSOR		("Processor")
+#define MACHINE			("MachineName")
+#define CRC				("CRC")
+
+namespace fs = std::filesystem;
+
+vr_update::vr_update(std::string Processor,std::string MachineName,
+           uint32_t Crc,std::string Model,uint16_t SlaveAddress,
+           std::string ConfigFilePath) : Processor(Processor),
+           MachineName(MachineName), Crc(Crc), Model(Model),
+           SlaveAddress(SlaveAddress), ConfigFilePath(ConfigFilePath)
+{
+
+    BusNumber = 0;
+    fd = FAILURE;
+
+}
+
+vr_update* vr_update::CreateVRFrameworkObject(std::string Model,
+              uint16_t SlaveAddress, uint32_t Crc, std::string Processor,
+              std::string MachineName, std::string configFilePath,std::string UpdateType)
+{
+	vr_update* p;
+
+    if ((strcasecmp(UpdateType.c_str(), PATCH)) == SUCCESS)
+    {
+        if ((Model.compare(RAA229613) == SUCCESS) ||
+             (Model.compare(RAA229625) == SUCCESS)||
+             (Model.compare(RAA229620) == SUCCESS)||
+             (Model.compare(ISL68220) == SUCCESS) ||
+             (Model.compare(RENESAS) == SUCCESS))
+        {
+            sd_journal_print(LOG_INFO,"Renesas patch update triggered\n");
+            p = new vr_update_renesas_patch(Processor,MachineName,Crc,Model,SlaveAddress,configFilePath);
+        }
+        else if (Model.compare(INFINEON_XDPE) == SUCCESS)
+        {
+           sd_journal_print(LOG_INFO,"XDPE patch update triggered\n");
+           /*TBD*/
+        }
+
+    }
+	else if ((Model.compare(RAA229613) == SUCCESS) ||
+	         (Model.compare(RAA229625) == SUCCESS)||
+             (Model.compare(RAA229620) == SUCCESS)){
+		p = new vr_update_renesas_gen3(Processor,MachineName,Crc,Model,SlaveAddress,configFilePath);
+	}
+
+	else if (Model.compare(ISL68220) == SUCCESS){
+		p = new vr_update_renesas_gen2(Processor,MachineName,Crc,Model,SlaveAddress,configFilePath);
+	}
+
+	else if(Model.compare(INFINEON_XDPE) == SUCCESS)
+    {
+		p = new vr_update_infineon_xdpe(Processor,MachineName,Crc,Model,SlaveAddress,configFilePath);
+    }
+    else if(Model.compare(INFINEON_TDA) == SUCCESS)
+    {
+        p = new vr_update_infineon_tda(Processor,MachineName,Crc,Model,SlaveAddress,configFilePath);
+    }
+	else{
+		sd_journal_print(LOG_ERR, "Invalid Framework\n");
+		return NULL;
+	}
+
+	return p;
+}
+
+bool vr_update::findBusNumber()
+{
+
+    /*Find bus number from the drivers binded*/
+    DIR *dir;
+    struct dirent *entry;
+    std::vector<std::string> slaveDevice;
+    std::string DeviceName;
+
+    std::stringstream ss;
+    ss << std::hex << SlaveAddress;
+    std::string SlaveAddrStr = ss.str();
+    const char* DriverPathStr = DriverPath.c_str();
+
+    if ((dir = opendir(DriverPathStr)) != NULL)
+    {
+        while ((entry = readdir(dir)) != NULL)
+        {
+            std::string fname = entry->d_name;
+
+            if(fname.find(SlaveAddrStr) != std::string::npos)
+            {
+                slaveDevice.push_back(fname);
+            }
+        }
+        closedir(dir);
+
+        if(slaveDevice.empty())
+        {
+            return false;
+        }
+        std::sort(slaveDevice.begin(),slaveDevice.end());
+
+        if(Processor.compare(SOCKET_0) == SUCCESS)
+        {
+            DeviceName = slaveDevice[INDEX_0];;
+            slaveDevice[INDEX_0].resize(INDEX_2);
+            BusNumber = std::stoi(slaveDevice[INDEX_0]);
+        }
+        else if(Processor.compare(SOCKET_1) == SUCCESS)
+        {
+            DeviceName = slaveDevice[INDEX_1];
+            slaveDevice[INDEX_1].resize(INDEX_2);
+            BusNumber = std::stoi(slaveDevice[INDEX_1]);
+        }
+    }
+
+    std::string UnbindDriver = "echo " + DeviceName + "> " + DriverPath + "unbind";
+
+    system(UnbindDriver.c_str());
+
+    if(BusNumber != 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool vr_update::openI2cDevice()
+{
+
+    char i2cDeviceName[FILE_PATH_SIZE];
+    bool rc = false;
+
+    std::snprintf(i2cDeviceName, FILE_PATH_SIZE, "/dev/i2c-%d", BusNumber);
+
+    fd = open(i2cDeviceName, O_RDWR);
+
+    if(fd != FAILURE)
+    {
+        if (ioctl(fd, I2C_SLAVE, SlaveAddress) != FAILURE)
+        {
+            rc = true;
+        }
+        else
+        {
+            sd_journal_print(LOG_ERR, "Error: Failed setting i2c dev addr\n");
+            rc = false;
+        }
+    } else {
+        sd_journal_print(LOG_ERR, "Error: failed to open VR device\n");
+        rc = false;
+    }
+
+    usleep(MIN_WAIT_TIME);
+    return rc;
+}
+
+void vr_update::closeI2cDevice()
+{
+    if (fd >= SUCCESS) {
+        close(fd);
+    }
+    fd = FAILURE;
+
+    std::stringstream ss;
+    ss << std::hex << SlaveAddress;
+    std::string SlaveAddrStr = ss.str();
+
+    std::string DeviceName = std::to_string(BusNumber) + "-00" + SlaveAddrStr;
+
+    std::string BindDriver = "echo " + DeviceName + " > " + DriverPath + "bind";
+
+    system(BindDriver.c_str());
+
+    sd_journal_print(LOG_INFO, "Binded driver back after VR update\n");
+
+}
