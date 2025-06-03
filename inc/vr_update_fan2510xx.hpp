1091bc62b483a8f70c6bbb93c5c06727c958e914
Abinaya Dhandapani <abinaya.dhandapani@amd.com>
2025-06-03 01:07:56 -0500

    Enable VR-update of FAN2510XX devices
    - Enabled VR update for FAN251015 and FAN251030
    VR devices.
    
    -  Updated Kenya and Nigeria VR config file with FAN2510XX devices.
    
    Signed-off-by: Abinaya Dhandapani <abinaya.dhandapani@amd.com>

---
+++ inc/vr_update_fan2510xx.hpp
@@ -0,0 +1,22 @@
+#ifndef VR_UPDATE_FAN2510XX_H_
+#define VR_UPDATE_FAN2510XX_H_
+
+#include "vr_update.hpp"
+
+#define USER_CRC_REG (0xCA)
+#define STORE_USER_ALL_CMD (0x15)
+
+class vr_update_fan2510xx : public vr_update
+{
+  public:
+    vr_update_fan2510xx(std::string Processor, uint32_t Crc, std::string Model,
+                        uint16_t SlaveAddress, std::string ConfigFilePath,
+                        std::string Revision, uint16_t PmbusAddress);
+
+    virtual bool crcCheckSum();
+    virtual bool isUpdatable();
+    virtual bool UpdateFirmware();
+    virtual bool ValidateFirmware();
+};
+
+#endif
